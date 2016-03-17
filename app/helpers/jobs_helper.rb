8d566295aab87f8a881dc1eef8668c39b97131a0
htrix <clio.trix@gmail.com>
2016-03-17 00:59:29 +0000

    layout improvement

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,6 +1,6 @@
 module JobsHelper
     def job_title(job)
-    title = raw("#{h(job.title)} - #{job.company.email}")
+    title = raw("#{h(job.title)} - #{job.company.name}")
 
     if job.prime
       title + content_tag(:strong, " (prime)")
