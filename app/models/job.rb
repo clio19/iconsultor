1ae5fd1f50418bfd3720880ebca7f0ce618cd512
clio19 <clio19@users.noreply.github.com>
2016-03-18 09:59:29 +0000

    Update job.rb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
     validates :title,:description, :presence => true
     belongs_to :company
-     #has_many :comments
+    
 
      scope :most_recent, -> { where(:prime => true).order("created_at DESC") }
 end
